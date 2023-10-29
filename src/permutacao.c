9226ac2c0db7d420b4496c49de33b0aae193fba9
Ed-Games <edmarcosfilho2015@gmail.com>
2023-10-28 21:36:28 -0300

    feat: adicionado parametro resultado para formar uma matriz com os caminhos possiveis, adicionado mostrarCombinacoes, para mostrar os caminhos e colocada lógica de trocar cada item na função ``trocar``

--- src/permutacao.c
+++ src/permutacao.c
@@ -1,32 +1,45 @@
-#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void printArr(int a[], int n) {
-    printf("---------------------------------------------------\n");
-    for (int i = 0; i < n; i++)
-        printf("%d ", a[i]);
-    printf("\n");
+// Função para imprimir as combinações
+void mostrarCombinacoes(int **result, int count, int n) {
+    printf("Todas as combinações:\n");
+    for (int i = 0; i < count; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-int heapPermutation(int a[], int size, int n) {
-    int count = 0;
+// Função para trocar os valores de dois inteiros
+void trocar(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
 
+// Função para preencher o array de ponteiros com as combinações
+int permutar(int a[], int size, int n, int **result, int count) {
     if (size == 1) {
-        printArr(a, n);
-        count++;
-        return count;
+        result[count] = (int *)malloc(n * sizeof(int));
+        if (result[count] == NULL) {
+            printf("Erro de alocação de memória.\n");
+            exit(1);
+        }
+        for (int i = 0; i < n; i++) {
+            result[count][i] = a[i];
+        }
+        return count + 1;
     }
 
     for (int i = 0; i < size; i++) {
-        count += heapPermutation(a, size - 1, n);
+        count = permutar(a, size - 1, n, result, count);
 
         if (size % 2 == 1) {
-            int temp = a[0];
-            a[0] = a[size - 1];
-            a[size - 1] = temp;
+            trocar(&a[0], &a[size - 1]);
         } else {
-            int temp = a[i];
-            a[i] = a[size - 1];
-            a[size - 1] = temp;
+            trocar(&a[i], &a[size - 1]);
         }
     }
 
